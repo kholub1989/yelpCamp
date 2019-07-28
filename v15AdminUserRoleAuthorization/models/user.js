7cae26f8d024b162f1652a29ec55e7284a2424e6
Krisztian Holub <kholub1989@gmail.com>
2019-07-28 13:36:28 -0400

    add v15 Admin User role authorization

---
+++ v15AdminUserRoleAuthorization/models/user.js
@@ -0,0 +1,15 @@
+var mongoose = require("mongoose");
+var passportLocalMongoose = require("passport-local-mongoose");
+
+var UserSchema = mongoose.Schema({
+   username: String,
+   password: String,
+   isAdmin: {
+      type: Boolean,
+      default: false
+   }
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("User", UserSchema);
